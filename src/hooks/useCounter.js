54f1edbac5c6ad4d1eb0ec85cdcbfe1538cbe2e6
SK <sarthakkarandikar03@gmail.com>
2023-01-10 00:42:59 +0000

    Jan 10, 2023, 6:12 AM

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,3 +1,11 @@
 const useCounter = () +> {
-    
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
 }
