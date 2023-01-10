08c86e2f3f90e64e37f9a579d4fd87b10c703ca6
SK <sarthakkarandikar03@gmail.com>
2023-01-10 00:54:10 +0000

    Jan 10, 2023, 6:24 AM

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -4,7 +4,7 @@ import useCounter from '../hooks/useCounter';
 import Card from './Card';
 
 const ForwardCounter = () => {
- const counter = useCounter()
+ const counter = useCounter(1)
 
   return <Card>{counter}</Card>;
 };
